17587e326d49299cac71b58daa9397402a028de5
umutesiwase yvette <rwjpyy@gmail.com>
2019-03-25 14:45:25 +0200

    added class in models.py

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+# Create your models here.
+
+
+class profile(models.Model):
+    city = models.CharField(max_length =30)
+    country = models.CharField(max_length =30) 
+    def __str__(self):
+        return self.country
+
+
+
+    
+class Comment(models.Model):
+    image_name  = models.CharField(max_length =30)
+    def __str__(self):
+        return self.name
+
+
+    @classmethod
+    def search_by_category(cls,name):
+        category = cls.objects.filter(image_name__icontains=image_name).first()
+        images=Image.objects.filter(category=category)
+        return images
+     
+class Follow(models.Model):
+    name= models.CharField(max_length =30)
+    country = models.CharField(max_length =30) 
+    def __str__(self):
+        return self.name
+
+
+
+
+
+class Image(models.Model):
+    image = models.CharField(max_length =30)
+    picture = models.ImageField(upload_to='photo/') 
+    like= models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    def __str__(self):
+        return self.image
--- photo/views.py
+++ photo/views.py
@@ -11,32 +11,19 @@ from .models import Image
 
 # Create your views here.
 def Instagram(request):
-    return render(request, 'Instagram.html')
+    return render(request, 'instagram.html')
   
 
 def photo_of_day(request):
       date = dt.date.today()
       photos = Image.objects.all()
-      return render(request, 'all-photo/todays-photo.html', {"date": date,"photos":photos})
+      return render(request, 'all-photo/todays-photo.html', {"date": date,"photo":photos})
 
 
-def image(request,image_id):
-    try:
+def request,image_id):
+    try:image(
         photo = Image.objects.get(id = image_id)
     except DoesNotExist:
         raise Http404()
     return render(request,"all-photo/image.html", {"image":photo})
-
-# def search_results(request):
-
-#     if 'category' in request.GET and request.GET["category"]:
-#         name = request.GET.get("category")
-#         searched_categories = Image.search_by_category(name)
-#         message = f"{name}"
-
-#         return render(request, 'all-photo/search.html',{"message":message,"categories": searched_categories})
-
-#     else:
-#         message = "You haven't searched for any term"
-#         return render(request, 'all-photo/search.html',{"message":message})
 
